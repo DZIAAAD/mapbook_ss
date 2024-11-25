a5beb12572508b2264ed0df2dde6ffc0093869f4
szymonbizunowicz <szymon.bizunowicz@student.wat.edu.pl>
2024-11-25 13:43:15 +0100

    chore: formating

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ users: list = [
 
 ]
 
-#TODO plaese update user list
-
 print(f'Witaj {users[0]['name']}!!')
 for user in users[1:]:
      print(f'Twój znajomy {user['name']}, z miasta {user['city']}, opublikował {user['posts']} postów')
