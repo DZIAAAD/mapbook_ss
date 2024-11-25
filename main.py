c9b3cd430dfba032b217ba19afd517114423ef22
szymonbizunowicz <szymon.bizunowicz@student.wat.edu.pl>
2024-11-25 13:42:11 +0100

    chore: added new users

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 users: list = [
-    {'name': 'Patrycja', 'posts': 1, 'city': 'Warszawa'},
+    {'name': 'Szymon z wąsem', 'posts': 1, 'city': 'Warszawa'},
     {'name': 'Dominik', 'posts': 3, 'city': 'Poznań'},
-    {'name': 'Szymon z wąsem', 'posts': 5, 'city': 'Toruń'},
+    {'name': 'Patrycja', 'posts': 5, 'city': 'Toruń'},
     {'name': 'Szymon', 'posts': 7, 'city': 'Łódź'},
     {'name': 'Patryk', 'posts': 9, 'city': 'Kielce'},
+    {'name': 'Żerom', 'posts': 11, 'city': 'Radom'},
+    {'name': 'Michał', 'posts': 13, 'city': 'Olsztyn'},
+    {'name': 'Dominik aka bax', 'posts': 15, 'city': 'Białystok'},
+    {'name': 'Kinga', 'posts': 17, 'city': 'Opole'},
+    {'name': 'Amelia', 'posts': 19, 'city': 'Wrocław'},
+
 ]
 
 #TODO plaese update user list
