9a339c677ca9da1b863379d525f1476ba4991f8c
DZIAAAD <szymon.szperka@student.wat.edu.pl>
2024-11-25 14:33:02 +0100

    feat: add read funcionality

---
+++ main.py
@@ -0,0 +1,9 @@
+from mapbook.users import users
+from mapbook.crud import hello,read_users
+
+def main():
+     hello(users[0]['Name'])
+     read_users(users)
+
+if __name__ == '__main__':
+     main()
