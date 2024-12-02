ee10a870662bfe9f00b2c7c546de22f40c5e1c40
DZIAAAD <szymon.szperka@student.wat.edu.pl>
2024-12-02 14:33:42 +0100

    feat: add remove user function

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -12,3 +12,10 @@ def add_user(users:list)->None:
     new_city:str=input('Proszę podać miasto nowego znajomego: ')
     new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
     users.append(new_user)
+
+def remove_user(users) -> None:
+     user_to_find: str = input('Podaj imię do usunięcia: ')
+     for user in users:
+         if user['Name'] == user_to_find:
+            print(f'usuwam: {user}')
+            users.remove(user)
