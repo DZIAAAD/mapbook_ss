60f1a336d59a4ef1b62ac33deff428db3c4a9f78
DZIAAAD <szymon.szperka@student.wat.edu.pl>
2024-12-02 12:58:06 +0100

    feat: create new user

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -5,3 +5,10 @@ def hello(users:str)->None:
 def read_users(users:list)->None:
     for user in users[1:]:
         print(f'Twój znajomy {user['Name']}, {user['city']}, opublikował {user['posts']} postów')
+
+def add_user(users:list)->None:
+    new_name:str=input('Proszę podać imię nowego znajomego: ')
+    new_posts:int=int(input('Proszę podać liczbę postów: '))
+    new_city:str=input('Proszę podać miasto nowego znajomego: ')
+    new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
+    users.append(new_user)
