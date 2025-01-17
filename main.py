67cc2740a2a7766241eeb866dbf85b029505f605
DZIAAAD <szymon.szperka@student.wat.edu.pl>
2025-01-17 11:20:45 +0100

    feat: add docer_composes with database and update show user function with dbscript

--- main.py
+++ main.py
@@ -7,18 +7,23 @@ from tkinter import *
 import requests
 from bs4 import BeautifulSoup
 import tkintermapview
+import psycopg2 as ps
+
+db_params = ps.connect(user='aaaaaaa',password='aaaaaaa',database='aaaaaaa',
+                       host='localhost',port='6432')
+
 
 
 
 
 
 class User:
-     def __init__(self,imie,nazwisko,postow,lokalizacja):
+     def __init__(self,imie,nazwisko,postow,lokalizacja,coordinates):
           self.imie = imie
           self.nazwisko = nazwisko
           self.postow = postow
           self.lokalizacja = lokalizacja
-          self.coords:list = User.get_coordinates(self)
+          self.coords:list = coordinates #User.get_coordinates(self)
           self.marker = map_widget.set_marker(
                self.coords[0],
                self.coords[1],
@@ -34,15 +39,23 @@ class User:
           ]
 
 
-#users=[
+users=[
 #      User('Kasia','Kowalska','7','Warszawa'),
 #      User('Adam','Nowak','2','Wrocław'),
-#]
+]
+
 
 def show_users():
+     cursor = db_params.cursor()
+     querry = "SELECT name,surname,posts,location,st_astext(coordinates),id FROM public.users ORDER BY id ASC "
+     cursor.execute(querry)
+     users_db = cursor.fetchall()
+     cursor.close()
      listbox_lista_obietktow.delete(0, END)
-     for idx, user in enumerate(users):
-          listbox_lista_obietktow.insert(idx,f'{user.imie} {user.nazwisko} {user.postow} {user.lokalizacja}')
+     for idx, user in enumerate(users_db):
+          User(user[0], user[1], user[2], user[3], [float(user[4][6:-1].split()[1]),float(user[4][6:-1].split()[0])]),
+          listbox_lista_obietktow.insert(idx,f'{user[0]} {user[1]} {user[2]} {user[3]}')
+
 
 def add_user()->None:
      name=entry_imie.get()
@@ -50,7 +63,7 @@ def add_user()->None:
      posts=entry_liczba_postow.get()
      location=entry_lokalizacja.get()
 
-     new_user=User(name,surname,posts,location)
+     new_user=User(name,surname,posts,location,coordinates = 11.0,20.1)
 
      users.append(new_user)
      show_users()
